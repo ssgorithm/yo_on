115cddbee7205383754c3e5abc30e219e467883d
jychoi210 <jychoi210@sookmyung.ac.kr>
2021-07-26 01:05:16 +0900

    크로아티아 알파벳(S5)_210724

---
+++ Implementation/src/S2941.java
@@ -0,0 +1,42 @@
+//백준 구현 Q2941 크로아티아 알파벳 (실버5)
+//https://www.acmicpc.net/problem/2941
+
+import java.io.*;
+
+public class S2941 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] words = br.readLine().toCharArray();
+        int size = words.length;
+        int count = 0;
+        for(int i = 0; i < size; i++){
+            char word = words[i];
+            if(word == 'c' && i < size - 1){
+                if(words[i+1] == '=' || words[i+1] == '-'){
+                    i++;
+                }
+            }
+            else if(word == 'd' && i < size - 1){
+                if(words[i+1] == 'z' && i < size - 2){
+                    if(words[i+2] == '=') {
+                        i += 2;
+                    }
+                }else if(words[i+1] == '-'){
+                    i++;
+                }
+            }
+            else if((word == 'l' || word == 'n') && i  < size - 1){
+                if(words[i+1] == 'j') {
+                    i++;
+                }
+            }
+            else if((word == 's' || word=='z') && i < size - 1){
+                if(words[i+1] == '='){
+                    i++;
+                }
+            }
+            count++;
+        }
+        System.out.print(count);
+    }
+}
