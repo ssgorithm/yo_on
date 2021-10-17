3489b9a5416a3998a7e90f27093de581eea1b96a
jychoi210 <jychoi210@sookmyung.ac.kr>
2021-10-17 21:45:12 +0900

    211016_문자열폭발(G4)

---
+++ DataStructure/src/G9935.java
@@ -0,0 +1,50 @@
+//백준 스택 Q9935 문자열폭발 (골드4)
+//https://www.acmicpc.net/problem/9935
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class G9935{
+    public static void main(String args[]) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        String bomb = reader.readLine();
+
+        String result = stringBomb(str,bomb);
+        System.out.println((result.length()==0) ? "FRULA" : result);
+    }
+
+    public static String stringBomb(String str, String bomb) {
+        Stack<Character> stack = new Stack<>();
+        int bombLen = bomb.length();
+
+        for(int i = 0; i < str.length(); i++){
+            stack.push(str.charAt(i));
+
+            if(stack.size() >= bombLen){
+                boolean flag = true;
+                for(int j = 0; j < bombLen; j++){
+                    if(stack.get(stack.size() - bombLen + j) != bomb.charAt(j)){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    for(int j = 0; j < bombLen; j++){
+                        stack.pop();
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char temp : stack){
+            sb.append(temp);
+        }
+
+        return sb.toString();
+    }
+
+}
+
